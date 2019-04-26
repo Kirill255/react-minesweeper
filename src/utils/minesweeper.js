b7878bf7cff0f3d49f802a8cad1d2ef0c98badec
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-04-26 20:35:37 +0300

    added addMineCounts

--- src/utils/minesweeper.js
+++ src/utils/minesweeper.js
@@ -11,9 +11,9 @@ export function startGame(params) {
     moves: 0,
     startedAt: Date.now()
   });
-  console.log(game);
+  console.log(addMineCounts(game));
 
-  return game;
+  return addMineCounts(game);
 }
 
 export function generateBoard({ cols, rows, mines }) {
@@ -28,4 +28,76 @@ export function generateBoard({ cols, rows, mines }) {
     .concat(mineCells)
     .sort(() => Math.random() - 0.5)
     .map((tile, idx) => tile.set("id", idx));
+}
+
+// добавляем кол-во мин вокруг каждой ячейки
+export function addMineCounts(game) {
+  const newBoard = game
+    .get("board")
+    .map((tile) => tile.set("mineCount", getMineCount(game, tile.get("id"))));
+
+  return game.set("board", newBoard);
+}
+
+export function getMineCount(game, tileId) {
+  const adjacentTiles = getAdjacentTiles(game, tileId);
+
+  return adjacentTiles.filter(isTileMine).size;
+}
+
+export function isTileMine(tile) {
+  return tile.get("isMine");
+}
+
+export function getAdjacentTiles(game, tileId) {
+  const adjacentTileIds = getAdjacentTileIds(game, tileId);
+
+  return adjacentTileIds.map((id) => game.getIn(["board", id]));
+}
+
+export function getAdjacentTileIds(game, tileId) {
+  return directions
+    .map((direction) => direction(game, tileId))
+    .toList()
+    .filter(isTileIdValid);
+}
+
+export function isTileIdValid(tileId) {
+  return !!tileId;
+}
+
+// направление вокруг ячейки похожее на компас https://c7.uihere.com/files/809/294/451/compass-euclidean-vector-compass.jpg
+const directions = new Map({
+  n: (game, tileId) => getTileId(game, tileId - game.get("cols")),
+  nw: (game, tileId) =>
+    isTileOnWEdge(game, tileId) ? null : getTileId(game, tileId - game.get("cols") - 1),
+  ne: (game, tileId) =>
+    isTileOnEEdge(game, tileId) ? null : getTileId(game, tileId - game.get("cols") + 1),
+  e: (game, tileId) => (isTileOnEEdge(game, tileId) ? null : getTileId(game, tileId + 1)),
+  se: (game, tileId) =>
+    isTileOnEEdge(game, tileId) ? null : getTileId(game, tileId + game.get("cols") + 1),
+  s: (game, tileId) => getTileId(game, tileId + game.get("cols")),
+  sw: (game, tileId) =>
+    isTileOnWEdge(game, tileId) ? null : getTileId(game, tileId + game.get("cols") - 1),
+  w: (game, tileId) => (isTileOnWEdge(game, tileId) ? null : getTileId(game, tileId - 1))
+});
+
+export function getTileId(game, tileId) {
+  if (tileId < 0 || tileId > game.get("cols") * game.get("rows") - 1) {
+    return null;
+  }
+
+  if (game.get("board").has(tileId)) {
+    return tileId;
+  }
+
+  return null;
+}
+
+export function isTileOnWEdge(game, tileId) {
+  return tileId % game.get("cols") === 0;
+}
+
+export function isTileOnEEdge(game, tileId) {
+  return tileId % game.get("cols") === game.get("cols") - 1;
 }
